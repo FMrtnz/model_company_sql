5f26e3743dea61a2c183edb0c01e2b08263ed7bb
FMrtnz <fab.mrtnz22@gmail.com>
2022-03-11 12:39:52 +0000

    Post a solution for FINANCES QUESTION Orders that have not yet been paid.

--- exo.sql
+++ exo.sql
@@ -41,26 +41,45 @@ ORDER BY MONTH(shippedDate) DESC;
 
 /*
 FINANCES QUESTION
-The turnover of the orders of the last two months by country.
 Orders that have not yet been paid.
 */
 SELECT
     o.customerNumber,
-    o.orderNumber,
     # Check and compare the numbers between the orders and the payment
     count(DISTINCT o.orderNumber) as nb_orders,
     count(DISTINCT p.checkNumber) as nb_payments,
-    count(DISTINCT o.orderNumber) - count(DISTINCT p.checkNumber) as diff
+    count(DISTINCT o.orderNumber) - count(DISTINCT p.checkNumber) as diff,
+    SUM(DISTINCT od.quantityOrdered * od.priceEach) as amount_order,
+    SUM(DISTINCT p.amount) as amount
 FROM orders as o
 # Get the payments data throught customersNumber
 LEFT JOIN payments as p ON p.customerNumber = o.customerNumber
-# Select only orders were not canceled
+LEFT JOIN orderdetails as od ON od.orderNumber = o.orderNumber
+# Select only ordersx were not canceled
 WHERE NOT o.status = "Cancelled"
 # Group BY customersNumber
-GROUP BY customerNumber
+GROUP BY o.customerNumber
 # Select Customers with more orders then payments
 HAVING diff > 0
 ORDER BY diff DESC;
+
+# Get the order and their total
+WITH sub_order AS (
+SELECT
+	o.customerNumber,
+	o.orderNumber,
+	SUM(DISTINCT od.quantityOrdered * od.priceEach) as amount_order
+FROM orders AS o
+LEFT JOIN orderdetails as od ON od.orderNumber = o.orderNumber
+GROUP BY o.orderNumber
+)
+SELECT * FROM orders
+# Reject orders that have a same customer number and same total than amout of payment
+WHERE NOT orderNumber IN (
+SELECT o.orderNumber FROM sub_order as o
+JOIN payments as p ON p.customerNumber = o.customerNumber
+WHERE p.amount = o.amount_order)
+AND NOT status = "cancelled";
 
 /*
 LOGISTIC QUESTION
